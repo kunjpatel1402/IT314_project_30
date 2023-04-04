549639ca55ba2cd0102df3f66a30b1a971955095
ayush0823 <ayushpatel0823@gmail.com>
2023-04-04 22:29:33 +0530

    mongodb connect through djongo

--- Code/project/project/settings.py
+++ Code/project/project/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'swe_test_db',
+        'CLIENT' : {
+            'host': 'mongodb+srv://superuser:superuser%40SWE30@swe-cluster.xxvswrz.mongodb.net/?retryWrites=true&w=majority',
+        }
     }
 }
 
