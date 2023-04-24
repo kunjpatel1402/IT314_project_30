0d4c35230e127ac4a0bf6d0fc4dabb281cfb4e09
Venil Gondaliya <venilpatel10@outlook.com>
2023-04-24 13:32:24 +0530

    updated post incident

--- Code/project/myApp/templates/myApp/static/myApp/postincident.js
+++ Code/project/myApp/templates/myApp/static/myApp/postincident.js
@@ -1,3 +1,93 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoidmVuaWwxMCIsImEiOiJjbGd0M3p3MHQxMDVyM3JvMm55Z3hjcjZiIn0.J76tfDJhVMVCDzXqNCpBIA';
+
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/venil10/clgt4gkwl002x01png8jhhn1f',
+  center: [72.627819, 23.188915],
+  zoom: 5
+});
+
+var searchInput = document.getElementById('search-input');
+var searchButton = document.getElementById('search-button');
+var place = document.getElementById('place');
+var err = document.getElementById('err');
+var longitude = document.getElementById('longitude');
+var latitude = document.getElementById('latitude');
+var marker;
+
+searchButton.addEventListener('click', function () {
+  geocode(searchInput.value);
+});
+
+function geocode(address) {
+  var url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=' + mapboxgl.accessToken;
+
+  fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+
+      if (data.features.length === 0) {
+        document.getElementsByName('Latitude')[0].value = null;
+        document.getElementsByName('Longitude')[0].value = null;
+        place.textContent = null;
+        err.textContent = "Location not found";
+        console.log("Location not found");
+        return;
+      }
+      // if(data==undefined) {
+      // console.log(data);
+      // console.log("venil");
+      // }  
+      // else {
+      var location = data.features[0].center;
+      var name = data.features[0].place_name;
+      place.textContent = name;
+      console.log(location[1], location[0])
+      longitude.textContent = 'Longitude: ' + location[0];
+      latitude.textContent = 'Latitude: ' + location[1];
+      document.getElementsByName('Latitude')[0].value = data.features[0].center[1];
+      document.getElementsByName('Longitude')[0].value = data.features[0].center[0];
+      err.textContent = null;
+      map.flyTo({
+        center: location,
+        zoom: 12
+      });
+      if (marker) {
+        marker.remove();
+      }
+      marker = new mapboxgl.Marker()
+        .setLngLat(location)
+        .addTo(map);
+    });
+}
+
+map.on('click', function (e) {
+  var coordinates = e.lngLat;
+  var url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + coordinates.lng + ',' + coordinates.lat + '.json?access_token=' + mapboxgl.accessToken;
+
+  fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      var name = data.features[0].place_name;
+      place.textContent = name;
+      console.log(coordinates.long, coordinates.lat);
+      longitude.textContent = 'Longitude: ' + coordinates.lng;
+      latitude.textContent = 'Latitude: ' + coordinates.lat;
+      document.getElementsByName('Latitude')[0].value = data.features[0].center[1];
+      document.getElementsByName('Longitude')[0].value = data.features[0].center[0];
+      err.textContent = null;
+      if (marker) {
+        marker.remove();
+      }
+      marker = new mapboxgl.Marker()
+        .setLngLat(coordinates)
+        .addTo(map);
+    });
+});
 
 
 function showOptions() {
@@ -24,3 +114,4 @@ function showOptions() {
     dropdown.add(option);
   }
 }
+
