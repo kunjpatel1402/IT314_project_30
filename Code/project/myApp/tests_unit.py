dff592f2ad14805e1ee91d722f00cfbb31d10b1c
ayush0823 <ayushpatel0823@gmail.com>
2023-04-27 23:18:06 +0530

    added sessions folder

--- Code/project/myApp/tests_unit.py
+++ Code/project/myApp/tests_unit.py
@@ -24,29 +24,28 @@ class LoginTestCase(TestCase):
     def test_login(self):
         response = self.client.post('/myApp/login/', {'UserName': 'testuser', 'Password': 'testpass'})
         self.assertEqual(response.status_code, 302)
-        response = self.client.get('/myApp/')
-        self.assertTemplateUsed(response, 'myApp/reg_hmpg.html')
+        self.assertEqual(response.url, '/myApp')
         self.client.get('/myApp/logout/')
         
     # test login failure
     def test_login_failure(self):
         response = self.client.post('/myApp/login/', {'UserName': 'testuser', 'Password': 'wrongpass'})
-        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'myApp/login.html')
 
     # test no input
     def test_login_no_input(self):
         response = self.client.post('/myApp/login/', {'UserName': '', 'Password': ''})
-        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'myApp/login.html')
 
     # test only username input
     def test_login_only_username(self):
         response = self.client.post('/myApp/login/', {'UserName': 'testuser', 'Password': ''})
-        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'myApp/login.html')
 
     # test only password input
     def test_login_only_password(self):
         response = self.client.post('/myApp/login/', {'UserName': '', 'Password': 'testpass'})
-        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'myApp/login.html')
 
 class RegisterTestCase(TestCase):
     @classmethod
@@ -78,48 +77,48 @@ class RegisterTestCase(TestCase):
         self.assertTemplateUsed(response, 'myApp/reg_hmpg.html')
         self.client.get('/myApp/logout/')
         response = self.client.post('/myApp/register/', {'UserName': 'testuser', 'Password': 'testpass', 'ConfirmPassword': 'testpass', 'Email': 'test@gmail.com', 'FirstName': 'test', 'LastName': 'user', 'DOB': '2002-07-20'})
-        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'myApp/register.html')
         self.collection.delete_many({'UserName': 'testuser'})
 
     # test no input
     def test_register_no_input(self):
         response = self.client.post('/myApp/register/', {'UserName': '', 'Password': '', 'ConfirmPassword': '', 'Email': '', 'FirstName': '', 'LastName': '', 'DOB': ''})
-        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'myApp/register.html')
 
     # test only username input
     def test_register_only_username(self):
         response = self.client.post('/myApp/register/', {'UserName': 'testuser', 'Password': '', 'ConfirmPassword': '', 'Email': '', 'FirstName': '', 'LastName': '', 'DOB': ''})
-        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'myApp/register.html')
         
     # test invalid email
     def test_register_invalid_email(self):
         response = self.client.post('/myApp/register/', {'UserName': 'testuser', 'Password': 'testpass', 'ConfirmPassword': 'testpass', 'Email': 'test', 'FirstName': 'test', 'LastName': 'user', 'DOB': '2002-07-20'})
-        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'myApp/register.html')
 
     # test password mismatch
     def test_register_password_mismatch(self):
         response = self.client.post('/myApp/register/', {'UserName': 'testuser', 'Password': 'testpass', 'ConfirmPassword': 'wrongpass', 'Email': 'test@gmail.com', 'FirstName': 'test', 'LastName': 'user', 'DOB': '2002-07-20'})
-        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'myApp/register.html')
 
 
 # post incident test case
 class PostIncidentTestCase(TestCase):
     @classmethod
-    def setUpClass(cls):
-        super(PostIncidentTestCase, cls).setUpClass()
-        cls.client = pymongo.MongoClient("mongodb+srv://superuser:superuser%40SWE30@swe-cluster.xxvswrz.mongodb.net/?retryWrites=true&w=majority")
-        cls.db = cls.client['swe_test_db']
-        cls.collection = cls.db['users']
-        cls.collection.insert_one({'UserName': 'testuser', 'Password': 'testpass', 'Email': 'test@gmail.com', 'FirstName': 'test', 'LastName': 'user', 'DOB': '2002-07-20'})
-        cls.collection = cls.db['incident']
+    def setUpClass(self):
+        super(PostIncidentTestCase, self).setUpClass()
+        self.client = pymongo.MongoClient("mongodb+srv://superuser:superuser%40SWE30@swe-cluster.xxvswrz.mongodb.net/?retryWrites=true&w=majority")
+        self.db = self.client['swe_test_db']
+        self.collection = self.db['users']
+        self.collection.insert_one({'UserName': 'testuser', 'Password': 'testpass', 'Email': 'test@gmail.com', 'FirstName': 'test', 'LastName': 'user', 'DOB': '2002-07-20'})
+        self.collection = self.db['incident']
 
     @classmethod
-    def tearDownClass(cls):
-        super(PostIncidentTestCase, cls).tearDownClass()
-        cls.collection = cls.db['users']
-        cls.collection.delete_many({'UserName': 'testuser'})
-        cls.collection = cls.db['incident']
-        cls.collection.delete_many({'title': 'test'})
+    def tearDownClass(self):
+        super(PostIncidentTestCase, self).tearDownClass()
+        self.collection = self.db['users']
+        self.collection.delete_many({'UserName': 'testuser'})
+        self.collection = self.db['incident']
+        self.collection.delete_many({'title': 'test'})
 
     # test post incident success after login assuming user is already registered
     def test_post_incident(self):
@@ -127,8 +126,10 @@ class PostIncidentTestCase(TestCase):
         self.assertEqual(response.status_code, 302)
         response = self.client.get('/myApp/postIncident/')
         self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'myApp/postIncident.html')
         response = self.client.post('/myApp/postIncident/', {'Title': 'test', 'Description': 'test', 'Latitude': '0', 'Longitude': '0', 'Time': '2021-04-20T00:00','crime-or-hazard': 'crime','IncidentType': 'Cybercrime'})
         self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, '/myApp')
         self.client.get('/myApp/logout/')
         self.collection.delete_many({'title': 'test'})
 
@@ -146,4 +147,5 @@ class PostIncidentTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         response = self.client.post('/myApp/postIncident/', {'Title': '', 'Description': '', 'Latitude': float(), 'Longitude': float(), 'Time': '','crime-or-hazard': '','IncidentType': ''})
         self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'myApp/postIncident.html')
         self.client.get('/myApp/logout/')
